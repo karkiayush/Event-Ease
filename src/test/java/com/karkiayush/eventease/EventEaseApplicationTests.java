f0a994f6c3151529a12d237b4d22e2c056a20dc8
karkiayush <karkiayush619@gmail.com>
2025-04-28 16:57:42 +0545

    Event-Ease project repo setup

---
+++ src/test/java/com/karkiayush/eventease/EventEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karkiayush.eventease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
