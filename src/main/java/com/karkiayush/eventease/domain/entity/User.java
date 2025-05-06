a3e4cf2b126c6e5147a86b193ac8c9ee8f968a6d
karkiayush <karkiayush619@gmail.com>
2025-05-06 22:08:38 +0545

    create user entity

---
+++ src/main/java/com/karkiayush/eventease/domain/entity/User.java
@@ -0,0 +1,30 @@
+package com.karkiayush.eventease.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+/* When we use @Data, Lombok automatically generates getter, setter, AllArgsConstructor, NoArgsConstructor, ToString & EqualsAndHashCode method.
+But since, we're going to implement bidirectional relationships. If we use @Data annotation, it generates EqualsAndHashCode for us, and in that case if the bidirectional relation exists, it will lead to the StackOverflow Issue. */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
