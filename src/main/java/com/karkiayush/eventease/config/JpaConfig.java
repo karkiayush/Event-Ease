94981de0a7ac21b6e87a1ea8b52155a604da166c
karkiayush <karkiayush619@gmail.com>
2025-05-11 14:22:35 +0545

    feat(JpaConfig): create orm.xml inside META-INF to enable the Spring JPA audit field annotation globally in project

--- src/main/java/com/karkiayush/eventease/config/JpaConfig.java
+++ src/main/java/com/karkiayush/eventease/config/JpaConfig.java
@@ -1,4 +1,13 @@
 package com.karkiayush.eventease.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
 public class JpaConfig {
+    // This class is used to enable JPA Auditing. It will automatically populate the created and updated date fields in the entity classes.
+    // We can also specify the date format here if we want to.
+    // But for now, we are not specifying any date format. So, it will use the default date format.
 }
